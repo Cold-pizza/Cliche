381b0f54654c70db09510d7f35b9a3682de3295c
Jay <originxh@gmail.com>
2021-12-18 15:51:25 +0900

    0.2.3 commit

--- src/components/version.tsx
+++ src/components/version.tsx
@@ -5,6 +5,12 @@ const Version = function () {
     <div id="version">
       <h1>*최신 버전입니다.*</h1>
       <section className="item">
+        <h1>ver - 0.2.3</h1>
+        <p className="path-title">버전내용</p>
+        <p className="path-content">- 노래추가시 부가설명 나오게 만들었습니다.</p>
+        <p className="path-content">- 곡 리스트에서 업로드 할 노래 선택해도 취소할 수 있게 만들었습니다. (뒤로가기 버튼)</p>
+      </section>
+      <section className="item">
         <h1>ver - 0.2.2</h1>
         <p className="path-title">버전내용</p>
         <p className="path-content">- 노래제거 기능 추가.</p>
